031bcf6cded66f598772df5902880f60d1627023
Charbel Kabro <charbel.kabro@gmail.com>
2025-02-13 19:30:59 +0100

    fix

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const GIST_ID = "3d33000b0f84d581ef0cffb58a43d86b";
 const GIST_URL = `https://api.github.com/gists/${GIST_ID}`;
-const encodedParts = [
-    "Z2l0aHViX3BhdF8xMUFGNTRMUlkwQVk1VHluNkU2b0tRX0J",
-    "NQzlhOGNzUVNmSFltOThtbDZ6QmZZbkJEUjhYTnJiTjR3b",
-    "VBLS0l4RHZXQTZNR1NEQjU2SXplME1J"
-];
+const encodedParts = "Z2l0aHViX3BhdF8xMUFGNTRMUlkwQVk1VHluNkU2b0tRX0JNQzlhOGNzUVNmSFltOThtbDZ6QmZZbkJEUjhYTnJiTjR3bVBLUEk4RHZXQTZNR1NEQjU2SXplME1J"
 
 // Mettre à jour le compteur de clics dans le Gist
 async function saveClickData(noClicks) {
@@ -19,12 +15,14 @@ async function saveClickData(noClicks) {
         await fetch(GIST_URL, {
             method: "PATCH",
             headers: {
-                "Accept": "application/vnd.github.v3+json",
-                "Authorization": `token ${atob(parts.join(""))}`
+                "Accept": "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2022-11-28",
+                "Authorization": `Bearer ${atob(encodedParts)}`
             },
             body: body
         });
         console.log("Données sauvegardées avec succès !");
+        window.location.href = "yes_page.html";
     } catch (error) {
         console.error("Erreur lors de l'enregistrement :", error);
     }
@@ -90,5 +88,4 @@ function handleNoClick() {
 
 function handleYesClick() {
     saveClickData(noClickCount);
-    window.location.href = "yes_page.html";
 }
